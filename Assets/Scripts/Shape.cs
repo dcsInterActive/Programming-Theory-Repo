8cfd0a012cbf9d2a13b3b670c5b4170afd0afeeb
dcsInterActive <101058026+dcsInterActive@users.noreply.github.com>
2022-03-11 17:48:04 -0600

    final commit
    3/11/22
    added Remarks to show four pillars of OOP

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// INHERITANCE
 public abstract class Shape : MonoBehaviour
 {
+    // ENCAPSULATION
     protected string m_name;
-
     public string Name
     {
         get { return m_name; }
@@ -16,11 +17,13 @@ public abstract class Shape : MonoBehaviour
         m_name = "Shape";
     }
 
+    // POLYMORPHISM
     public virtual int GetSides()
     {
         return 0;
     }
 
+    // ABSTRACTION
     public string GetMaterial()
     {
         Material m = gameObject.GetComponent<Renderer>().material;
